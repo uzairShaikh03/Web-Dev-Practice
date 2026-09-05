9e2920d766bc0cd971772ab9e3d3fdb9b3008af0
Uzair Shaikh <uzair.shaikh1207@gmail.com>
2026-09-04 21:54:59 -0400

    daily

---
+++ Js exercises/randompay.js
@@ -0,0 +1,14 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    
+ var random= Math.random();
+ random= Math.floor(random*5)+1;
+ return names[random];
+    
+
+/******Don't change the code below*******/    
+}
+whosPaying(["Angela", "Ben", "Jenny", "Michael", "Chloe"]);
